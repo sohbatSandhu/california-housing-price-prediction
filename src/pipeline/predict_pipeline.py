81670a2a7d07d180a81a1041d966034b4f2800fe
sohbatSandhu <sohbatsandhu14@gmail.com>
2024-09-09 23:18:39 -0700

    implement prediction pipeline

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -9,18 +9,48 @@ class PredictionPipeline:
     
     def predict(self, features):
         try:
+            # define paths to data preprocessing objects and models
             MODEL_PATH = 'artifacts/model.pkl'
+            IMPUTER_PATH = 'artifacts/imputer.pkl'
+            FEAT_ENGINEERING_PATH = 'artifacts/featengineering.pkl'
+            LOG_TRANSFORMER_PATH = 'artifacts/logtransformer.pkl'
             PREPROCESSOR_PATH = 'artifacts/preprocessor.pkl'
+            
+            # load objects
             model = load_object(file_path = MODEL_PATH)
+            imputer = load_object(file_path= IMPUTER_PATH)
+            feat_engineer = load_object(file_path= FEAT_ENGINEERING_PATH)
+            transformer = load_object(file_path= LOG_TRANSFORMER_PATH)
             preprocessor = load_object(file_path= PREPROCESSOR_PATH)
-            data_scaled = preprocessor.transform(features)
+            
+            # perform imputation
+            imputed_data = pd.DataFrame(
+                imputer.fit_transform(features), columns=features.columns
+            )
+            # Manually convert data types back to original
+            numerical_features = [
+                'longitude',
+                'latitude',
+                'housing_median_age',
+                'total_bedrooms',
+                'total_rooms',
+                'population',
+                'households',
+                'median_income'
+            ]
+            for column in numerical_features:
+                imputed_data[column] = imputed_data[column].astype(float)
+            
+            engineered_data = feat_engineer.transform(imputed_data)
+            engineered_transformed_data = transformer.transform(engineered_data)
+            data_scaled = preprocessor.transform(engineered_transformed_data)
             preds = model.predict(data_scaled)
             return preds
         
         except Exception as e:
             raise CustomException(e, sys)
 
-class CustomerData:
+class HousingData:
     def __init__(self,
         longitude: float,
         latitude : float,
